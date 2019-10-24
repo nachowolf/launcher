f55723932b1d4828f1d6337ac8339acd8765c93d
nachowolf <nathrijacobs@gmail.com>
2019-10-24 12:19:42 +0200

    alert box added

--- public/javascript/dom.js
+++ public/javascript/dom.js
@@ -1,19 +1,25 @@
 const launch = document.querySelectorAll(".launch");
 const status = document.querySelectorAll(".status");
 const reset = document.querySelector("#reset");
+const missle = document.querySelectorAll(".missle");
 
 
 for (let x = 0; x< launch.length; x++){
     let button = launch[x]
+    let s = missle[x].innerHTML
     button.addEventListener("click", function(){
         if(status[x].id == "online"){
             status[x].innerHTML = "offline";
             status[x].id = "offline"
+                //`$(missle[x].innerHTML) has been launched`
+            alert(`${s} missle has been launched`);
         }
 
         else if (status[x].id == "offline"){
+            alert(`${s} missle has malfunctioned`);
             status[x].innerHTML = "malfunction";
-            status[x].id = "malfunction"
+            status[x].id = "malfunction";
+           
         }
     })
 }
